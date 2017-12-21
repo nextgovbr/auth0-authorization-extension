565e240974ffc3a02bd8eb44ca297fafe7048c53
Cristofer Gonzales <crigot@hotmail.com>
2017-12-21 11:15:28 -0300

    Adds delay while loading modules

--- webtask.js
+++ webtask.js
@@ -1,7 +1,6 @@
 const url = require('url');
 const tools = require('auth0-extension-hapi-tools');
 
-const hapiApp = require('./server/init');
 const config = require('./server/lib/config');
 const logger = require('./server/lib/logger');
 const webtask = require('./server/lib/webtask');
@@ -15,15 +14,29 @@ tools.urlHelpers.getBaseUrl = (req) => {
   });
 };
 
-const createServer = tools.createServer((wtConfig, wtStorage) => {
+const factory = (wtConfig, wtStorage) => {
   logger.info('Starting Authorization Extension - Version:', process.env.CLIENT_VERSION);
   logger.info(' > WT_URL:', wtConfig('WT_URL'));
   logger.info(' > PUBLIC_WT_URL:', config('PUBLIC_WT_URL'));
-  return hapiApp(wtConfig, wtStorage);
-});
+  // Require in place to load the dependency only when needed
+  // and avoid Blocked event loop errors
+  return require('./server/init')(wtConfig, wtStorage);
+};
 
+  // Loading all modules at the beginning takes too much time
+  // that causes "Blocked event loop errors"
+  // This function is a helper to avoid this type of errors
+var createServer = (context, req, res) => {
+  // To avoid the  "Blocked event loop" error we delay loading the application module
+  setImmediate(() => {
+    config.setValue('PUBLIC_WT_URL', webtask.getUrl(req));
+    // After the application has been initialized we remove the 
+    // artificial delay in processing
+    createServer = tools.createServer(factory);
+    createServer(context, req, res);
+  });
+};
 
 module.exports = (context, req, res) => {
-  config.setValue('PUBLIC_WT_URL', webtask.getUrl(req));
   createServer(context, req, res);
 };
