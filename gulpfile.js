f49f1a663b9b4704a5363ffda8d2d32d93476bf0
Douglas Gabriel Cardoso <32394842+douglasgc@users.noreply.github.com>
2019-11-03 14:01:32 -0300

    fix(erro gulpfile.js): Gulpfile estava dando erro e impedindo servir como develop

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,7 @@ const gulp = require('gulp');
 const util = require('gulp-util');
 const open = require('open');
 const nodemon = require('gulp-nodemon');
-
-import configNext from '../configNext';
+const configNext = require('./configNext');
 
 gulp.task('run', () => {
   nodemon({
